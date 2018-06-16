cef3f1dbb97c23af2340695eeb75487b248b8b7f
Xinli Zou <xlzou@live.com>
2018-06-15 22:23:43 -0700

    New walker is added

---
+++ Assets/Scripts/LegJointController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(HingeJoint))]
+public class LegJointController : MonoBehaviour
+{
+    private HingeJoint leg;
+    private HingeJoint foreLeg;
+
+	// Use this for initialization
+	void Start () {
+        leg = GetComponent<HingeJoint>();
+        foreLeg = transform.GetChild(0).GetComponent<HingeJoint>();
+	}
+
+    public IEnumerator _MoveDown()
+    {
+        while (true)//this.leg.angle < 90)
+        {
+            this.leg.motor = new JointMotor() { force = 10010, targetVelocity = 40 };
+            this.foreLeg.motor = new JointMotor() { force = 10000, targetVelocity = 0 };
+            yield return -1;
+        }
+        Debug.Log("Finished moving down");
+        this.leg.motor = new JointMotor() { force = 0, targetVelocity = 0 };
+    }
+	
+}
---
+++ Assets/Scripts/Walker4StrockController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walker4StrockController : MonoBehaviour
+{
+    public LegJointController legL0;
+    public LegJointController legR0;
+    public LegJointController legL1;
+    public LegJointController legR1;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(_startProcedure());
+	}
+
+    private IEnumerator _startProcedure()
+    {
+        yield return new WaitForSeconds(1);
+
+        var c0 = StartCoroutine(legL0._MoveDown());
+        var c1 = StartCoroutine(legR0._MoveDown());
+        var c2 = StartCoroutine(legL1._MoveDown());
+        var c3 = StartCoroutine(legR1._MoveDown());
+        yield return c0;
+        yield return c1;
+        yield return c2;
+        yield return c3;
+    }
+}
